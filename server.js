c0e6f8deff9735cd8ffdb7289d287e372145aef6
Jenny Johns <johns.j@husky.neu.edu>
2018-10-29 03:16:45 -0400

    Adding server file

---
+++ server.js
@@ -0,0 +1,79 @@
+/**
+ * Created by sesha on 6/2/17.
+ */
+
+// Get the dependencies
+
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+const app = express();
+const cookieParser = require('cookie-parser');
+const session      = require('express-session');
+const passport = require('passport');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+app.use(session({ secret: process.env.SESSION_SECRET }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// Point static path to dist -- For building -- REMOVE
+app.use(express.static(path.join(__dirname, 'dist')));
+
+
+
+// CORS
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   next();
+// });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin",
+    "http://localhost:4200");
+  res.header("Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
+
+
+
+const port = process.env.PORT || '3100';
+app.set('port', port);
+
+
+// Create HTTP server
+const server = http.createServer(app);
+
+var serverSide = require("./server/app");
+serverSide(app);
+
+
+
+// For Build: Catch all other routes and return the index file -- BUILDING
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+
+server.listen( port , () => console.log('Running'));
+
+// require("./server/app");
+// app.listen(port, ipaddress);
+//
+// //install, load, and configure body parser module
+// app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.json());
+
+
